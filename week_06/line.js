370a3059dbbdbdadc1dcee40c976481f9ae8069c
lyuxueyue <82036413+lyuxueyue@users.noreply.github.com>
2022-11-08 00:26:17 -0600

    upload js files and index html

---
+++ week_06/line.js
@@ -0,0 +1,77 @@
+/* multiline plot for shot proportion by races */
+
+let height = 200,
+    width = 400,
+    margin = ({ top: 25, right: 55, bottom: 35, left: 30 })
+    innerWidth = width - margin.left - margin.right;
+
+const svg = d3.select("#chart2")
+  .append("svg")
+  .attr("viewBox", [0, 0, width, height]);
+
+d3.csv("race_month_num.csv").then(data => {
+  let timeParse = d3.timeParse("%Y-%m");
+
+  let races = new Set();
+
+  for (let d of data) {
+    d.Date = timeParse(d.Date);
+    d.Num = +d.Num;
+    races.add(d.Race); // push unique values to Set
+  }
+
+  let x = d3.scaleTime()
+    .domain(d3.extent(data, d => d.Date))
+    .range([margin.left, width - margin.right]);
+
+  let y = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.Num)).nice()
+    .range([height - margin.bottom, margin.top]);
+
+  svg.append("g")
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x).tickSizeOuter(0).tickSizeInner(0));
+
+  svg.append("g")
+    .attr("transform", `translate(${margin.left},0)`)
+    .attr("class", "y-axis")
+    .call(d3.axisLeft(y).tickSize(-innerWidth).tickFormat(d => d + "%"));
+
+  let line = d3.line()
+    .x(d => x(d.Date))
+    .y(d => y(d.Num));
+ 
+  for (let race of races) {
+    // .filter to filter data in D3
+    let raceData = data.filter(d => d.Race === race);
+
+    let g = svg.append("g")
+      .attr("class", "race")
+      .on('mouseover', function () {
+        // set/remove highlight class
+        d3.selectAll(".highlight").classed("highlight", false);
+        d3.select(this).classed("highlight", true);
+      });
+
+    if (race === "Black") {
+      g.classed("highlight", true);
+    }
+
+    g.append("path")
+      .datum(raceData)
+      .attr("fill", "none")
+      .attr("stroke", "#ccc")
+      .attr("d", line)
+
+    let lastEntry = raceData[raceData.length - 1]; //last piece of data to position text x and y
+
+    g.append("text")
+      .text(race)
+      .attr("x", x(lastEntry.Date))
+      .attr("y", y(lastEntry.Num))
+      .attr("dx", "5px") // shifting attribute in svg
+      .attr("dominant-baseline", "middle")
+      .attr("fill", "#999");
+  }
+  
+});
---
+++ week_06/ring.js
@@ -0,0 +1,62 @@
+/* Multi-ring plot */
+d3.json('race_gender_num.json').then((data) => {
+    for (let d of data) {
+      createRing(d);
+    }
+  });
+  
+  function createRing({ race, values }) {
+    const height = 350,
+      width = 400,
+      innerRadius = 40,
+      outerRadius = 65,
+      labelRadius = 85;
+  
+    const arcs = d3.pie().value(d => d.num)(values);
+  
+    const arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius);
+  
+    const arcLabel = d3.arc().innerRadius(labelRadius).outerRadius(labelRadius);
+  
+    const svg = d3.select("#chart3")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [-width / 2, -height / 2, width, height])
+      .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+  
+    svg.append("g")
+      .attr("stroke", "white")
+      .attr("stroke-width", 2)
+      .attr("stroke-linejoin", "round")
+      .selectAll("path")
+      .data(arcs)
+      .join("path")
+      .attr("fill", (d, i) => d3.schemeCategory10[i])
+      .attr("d", arc);
+  
+    svg.append("g")
+      .attr("font-size", 14)
+      .attr("text-anchor", "middle")
+      .selectAll("text")
+      .data(arcs)
+      .join("text")
+      .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
+      .selectAll("tspan")
+      .data(d => {
+        return [d.data.gender, d.data.num];
+      })
+      .join("tspan")
+      .attr("x", 0)
+      .attr("y", (d, i) => `${i * 1.1}em`)
+      .attr("font-weight", (d, i) => i ? null : "bold")
+      .text(d => d);
+  
+    svg.append("text")
+      .attr("font-size", 16)
+      .attr("font-weight", "bold")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .text(race)
+      .style("font-size", 20);
+  }
